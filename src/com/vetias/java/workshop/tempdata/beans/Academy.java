a791b06ebf15a5f26060dec3e20998ac2dfdce40
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-03 15:10:36 +0530

    builddata

---
+++ src/com/vetias/java/workshop/tempdata/beans/Academy.java
@@ -0,0 +1,7 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Academy extends Build {
+    private boolean isLibraryAvailable;
+    private boolean isAuditorimAvailable;
+    
+}
---
+++ src/com/vetias/java/workshop/tempdata/beans/Hostal.java
@@ -0,0 +1,6 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Hostal extends Build{
+    private boolean isMessAvailable;
+    private int numberofroom;
+}
