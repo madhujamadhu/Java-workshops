532724a550a887f43052d77a5dc3f8f8b913ac72
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-03 14:16:52 +0530

    temdata

---
+++ src/com/vetias/java/workshop/tempdata/beans/Build.java
@@ -0,0 +1,19 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Build {
+    private String name;
+    private String area;
+     public void Name(String aName){
+        name=aName;
+    }
+    public String getdName(){
+        return name;
+    }
+     public void setarea(String aarea){
+        area=aarea;
+    }
+    public String area(){
+        return area;
+    }
+    
+}
