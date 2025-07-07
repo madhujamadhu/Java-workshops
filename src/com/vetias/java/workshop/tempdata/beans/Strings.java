3bd5b47bb531a3d59d593a185e90eba446b7df03
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-07 13:23:16 +0530

    string type

---
+++ src/com/vetias/java/workshop/tempdata/beans/Strings.java
@@ -0,0 +1,12 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Strings {
+    public static void main(String[] args) {
+        String firstname="madhuja";
+        String lastname="madhu";
+        String finalname=firstname+" "+lastname;
+        System.out.println(finalname);
+        System.out.println(finalname.toUpperCase());
+    }
+    
+}
