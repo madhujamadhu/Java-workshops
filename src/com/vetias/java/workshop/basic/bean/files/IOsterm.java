3abb9fe0a3be1522bafdcfc934eac20e0067b16e
madhu <madhujak24cse@vetias.ac.in>
2025-07-09 20:24:11 +0530

    files

---
+++ src/com/vetias/java/workshop/basic/bean/files/IOsterm.java
@@ -0,0 +1,15 @@
+public class IOsterm {
+    public static void main(String[] args) {
+        try(FileInputStream inputStream =new FileInputStream("km.jpeg");
+        FileOutputStream outputStream = new FileOutputStream("madhu.jpeg")){
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+}
