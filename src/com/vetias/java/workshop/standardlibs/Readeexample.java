8e282513c6fc9c21bab52b781fa444807161f921
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-09 10:07:23 +0530

    filereading

---
+++ src/com/vetias/java/workshop/standardlibs/Readeexample.java
@@ -0,0 +1,21 @@
+package src.com.vetias.java.workshop.standardlibs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Readeexample {
+    public static void main(String[] args) {
+        try (BufferedReader reader=new BufferedReader(new FileReader("README.md"))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            
+        }
+    }
+
+    
+}
